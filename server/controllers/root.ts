a3eab1b528179327ff43e6b94d312513c046edaf
Zunawe <gyroscope15@gmail.com>
2021-04-18 10:19:41 -0700

    Fix inverted error check

--- server/controllers/root.ts
+++ server/controllers/root.ts
@@ -5,7 +5,7 @@ import { logger } from '../util'
 
 export const get: RequestHandler = (req, res) => {
   res.sendFile(path.join(process.cwd(), 'dist', 'client', 'index.html'), (err) => {
-    if (err === undefined) {
+    if (err !== undefined) {
       logger.error(err)
     }
   })
